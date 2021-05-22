3d7a59aac6498c654dea59f0b6e5167f481f6a16
GuilhermePCastro <guibkk@gmail.com>
2021-05-21 21:27:37 -0300

    Aula e exercicio

--- Aula/routes/api.php
+++ Aula/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FuncionariosController;
 use App\Http\Controllers\VendasController;
+use App\Http\Controllers\APIController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,7 +21,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::prefix('v1')->group(static function(){
+Route::group(['middleware' => 'auth.jwt', 'prefix' => 'v1'], function(){
 
     Route::get('/funcionarios', [FuncionariosController::class, 'index'])->name('funcionarios.index');
     Route::get('/funcionarios/{id}', [FuncionariosController::class, 'show'])->name('funcionarios.show');
@@ -29,7 +30,8 @@ Route::prefix('v1')->group(static function(){
     Route::delete('/funcionarios/{id}', [FuncionariosController::class, 'destroy'])->name('funcionarios.destroy');
 });
 
-Route::prefix('v1')->group(static function(){
+//Exercicio 21/05
+Route::group(['middleware' => 'auth.jwt', 'prefix' => 'v1'], function(){
 
     Route::get('/vendas', [VendasController::class, 'index'])->name('vendas.index');
     Route::get('/vendas/{id}', [VendasController::class, 'show'])->name('vendas.show');
@@ -37,3 +39,6 @@ Route::prefix('v1')->group(static function(){
     Route::post('/vendas', [VendasController::class, 'store'])->name('vendas.store');
     Route::delete('/vendas/{id}', [VendasController::class, 'destroy'])->name('vendas.destroy');
 });
+
+Route::post('login', [APIController::class, 'login'])->name('api.login');
+Route::post('logout', [APIController::class, 'logout'])->name('api.logout');
